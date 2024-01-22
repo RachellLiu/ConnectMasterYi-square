59ea577a3577e7d93c8f09d3679fcf78f207a0d6
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2024-01-22 13:49:42 +0000

    added a random move method

--- src/main/java/com/thg/accelerator23/connectn/ai/ywyz/ConnectMasterYiSquare.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ywyz/ConnectMasterYiSquare.java
@@ -3,6 +3,9 @@ package com.thg.accelerator23.connectn.ai.ywyz;
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
+import com.thehutgroup.accelerator.connectn.player.Position;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class ConnectMasterYiSquare extends Player {
@@ -15,6 +18,7 @@ public class ConnectMasterYiSquare extends Player {
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+    int randomNum = ThreadLocalRandom.current().nextInt(1, 10);
+    return randomNum;
   }
 }
