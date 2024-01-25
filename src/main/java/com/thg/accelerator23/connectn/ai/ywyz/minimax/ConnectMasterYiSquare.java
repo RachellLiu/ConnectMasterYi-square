15a4d233656eef821ae7566f68502dc6e2aaf558
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2024-01-25 11:49:21 +0000

    modified the maxdepth to 5

--- src/main/java/com/thg/accelerator23/connectn/ai/ywyz/minimax/ConnectMasterYiSquare.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ywyz/minimax/ConnectMasterYiSquare.java
@@ -19,7 +19,7 @@ public class ConnectMasterYiSquare extends Player {
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
 //    int randomNum = ThreadLocalRandom.current().nextInt(1, 11);
 //    return randomNum;
-    MinMax ai = new MinMax(2, getCounter());
+    MinMax ai = new MinMax(5, getCounter());
     MinimaxState thisState = new MinimaxState(board, 1,0);
     return ai.decision(thisState);
   }
